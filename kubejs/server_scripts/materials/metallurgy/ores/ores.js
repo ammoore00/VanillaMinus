72d36db3028b8e53023999eb2d70178e10fd8aa7
ammoore00 <abigailmmoore00@gmail.com>
2024-02-08 22:40:33 -0700

    Changed world gen mods

--- kubejs/server_scripts/materials/metallurgy/ores/ores.js
+++ kubejs/server_scripts/materials/metallurgy/ores/ores.js
@@ -1,15 +1,2 @@
 ServerEvents.tags('block', event => {
-    event.remove('minecraft:needs_stone_tool', [
-        "minecraft:iron_block",
-        "minecraft:raw_iron_block",
-        "minecraft:iron_ore",
-        "minecraft:deepslate_iron_ore"
-    ])
-
-    event.add('minecraft:needs_iron_tool', [
-        "minecraft:iron_block",
-        "minecraft:raw_iron_block",
-        "minecraft:iron_ore",
-        "minecraft:deepslate_iron_ore"
-    ])
 })
