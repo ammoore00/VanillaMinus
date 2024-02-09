ca420b4f877261b9aa6c778f995990b7afd53c25
ammoore00 <abigailmmoore00@gmail.com>
2024-02-08 19:49:45 -0700

    Added globe recipe

--- kubejs/server_scripts/building/decoration.js
+++ kubejs/server_scripts/building/decoration.js
@@ -1,3 +1,15 @@
 ServerEvents.recipes(event => {
-    
+    event.shaped(
+        'supplementaries:globe',
+        [
+            'WR',
+            'RR',
+            'SS'
+        ],
+        {
+            W: '#minecraft:wool',
+            R: 'createaddition:gold_rod',
+            S: 'create:golden_sheet'
+        }
+    ).id('btw:globe')
 })
