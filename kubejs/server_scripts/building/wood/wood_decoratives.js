ebcbdee04186cec99b3226586ef3e2a120b0d3fc
ammoore00 <abigailmmoore00@gmail.com>
2024-04-03 17:05:16 -0600

    Small recipe tweaks

--- kubejs/server_scripts/building/wood/wood_decoratives.js
+++ kubejs/server_scripts/building/wood/wood_decoratives.js
@@ -1,3 +1,4 @@
 ServerEvents.recipes(event => {
-
+    event.replaceInput({output: 'quark:azalea_hedge'}, '#minecraft:oak_logs', '#ecologics:azalea_logs')
+    event.replaceInput({output: 'quark:flowering_azalea_hedge'}, '#minecraft:oak_logs', '#ecologics:flowering_azalea_logs')
 })
